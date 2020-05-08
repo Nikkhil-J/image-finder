80e5a89dd65d64fe90dd31c438e7d042aa90a8ed
Nikkhil-J <nikhil.bits02@gmail.com>
2020-05-08 20:34:18 +0530

    final version

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import AppBar from 'material-ui/AppBar';
+
+function Navbar() {
+    return (
+        <AppBar title="Nikhil's Pixabay Image Finder"/>
+    )
+}
+
+export default Navbar
